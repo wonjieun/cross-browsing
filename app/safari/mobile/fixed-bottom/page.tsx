8d786694b5aa3c9c6d975d4ad2da6fca80d0ab89
Jina <wonjieunkk@gmail.com>
2023-12-12 22:51:59 +0900

    🐛 FIX: --vh custom property

--- app/safari/mobile/fixed-bottom/page.tsx
+++ app/safari/mobile/fixed-bottom/page.tsx
@@ -7,7 +7,7 @@ export default function FixedBottom() {
   useEffect(() => {
     const setVh = () => {
       vh.current = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty('--vh', `${vh}px`);
+      document.documentElement.style.setProperty('--vh', `${vh.current}px`);
     }
 
     window.addEventListener('resize', setVh);
@@ -17,18 +17,18 @@ export default function FixedBottom() {
   }, [])
 
   return (
-    <main className="page-container flex flex-col" style={{
+    <main className="page-container flex h-screen flex-col border" style={{
       height: "calc(var(--vh, 1vh) * 100)"
     }}>
-      <article className="page-body-container">
+      <article className="page-body-container overflow-y-scroll">
         Body Top
-        <section className="page-body">
+        <section className="page-body min-h-[2000px]">
           Body Middle
         </section>
         Body Bottom
       </article>
 
-      <footer className="page-footer">
+      <footer className="page-footer w-full px-4 py-2 border">
         Fixed Bottom
       </footer>
     </main>

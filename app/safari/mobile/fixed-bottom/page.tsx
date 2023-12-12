75ef40208da29f8162cd1e4f452a2f7a128d96a8
Jina <wonjieunkk@gmail.com>
2023-12-12 22:21:43 +0900

    📦 NEW: Impl fixed-bottom page

--- app/safari/mobile/fixed-bottom/page.tsx
+++ app/safari/mobile/fixed-bottom/page.tsx
@@ -1,8 +1,31 @@
+'use client'
+import { useEffect, useRef } from "react";
+
 export default function FixedBottom() {
+  const vh = useRef(0);
+
+  useEffect(() => {
+    const setVh = () => {
+      vh.current = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }
+
+    window.addEventListener('resize', setVh);
+    setVh();
+
+    return () => window.removeEventListener('resize', setVh);
+  }, [])
+
   return (
-    <main className="page-container flex flex-col">
+    <main className="page-container flex flex-col" style={{
+      height: "calc(var(--vh, 1vh) * 100)"
+    }}>
       <article className="page-body-container">
-        <section className="page-body">Body</section>
+        Body Top
+        <section className="page-body">
+          Body Middle
+        </section>
+        Body Bottom
       </article>
 
       <footer className="page-footer">
